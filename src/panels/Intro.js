22e723b9f0699ecdc068c132a71aaf4aeb693eba
CapUlDis <capuldis@gmail.com>
2020-09-04 16:02:52 +0300

    Fix position and used components for Intro.

--- src/panels/Intro.js
+++ src/panels/Intro.js
@@ -1,8 +1,8 @@
 import React, { Fragment } from 'react';
 import Panel from '@vkontakte/vkui/dist/components/Panel/Panel';
-import Group from '@vkontakte/vkui/dist/components/Group/Group';
 import Div from '@vkontakte/vkui/dist/components/Div/Div';
 import FixedLayout from '@vkontakte/vkui/dist/components/FixedLayout/FixedLayout';
+import Placeholder from '@vkontakte/vkui/dist/components/Placeholder/Placeholder';
 import Button from '@vkontakte/vkui/dist/components/Button/Button';
 import card1 from '../img/Intro-card-1.png';
 import card2 from '../img/Intro-card-2.png';
@@ -14,14 +14,16 @@ const Intro = ({ id, snackbarError, fetchedUser, userHasSeenIntro, go }) => {
 		<Panel id={id} centered={true}>
 			{(!userHasSeenIntro && fetchedUser) &&
 				<Fragment>
-					<Group>
-						<Div className='About'>
-							<img src={card1} alt='Промо карточка 1' className='Card1'/>
-							<img src={card2} alt='Промо карточка 2' className='Card2'/>
-							<h2>Создавайте счётчики для любых событий</h2>
-							<p>Вы можете следить за тем, сколько осталось до события или сколько уже прошло времени после него</p>
-						</Div>
-					</Group>
+					<Div className='about'>
+						<img src={card1} alt='Промо карточка 1' className='card1'/>
+						<img src={card2} alt='Промо карточка 2' className='card2'/>
+						<Placeholder 
+							header="Создавайте счетчики для любых событий" 
+							className='placeholder'
+							>
+							Вы можете следить за тем, сколько осталось до события или сколько уже прошло времени после него
+						</Placeholder>
+					</Div>
 					<FixedLayout vertical='bottom'>
 						<Div>
 							<Button mode='commerce' size='xl' onClick={go}>
