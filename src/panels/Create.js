26259a7a005c9a127f8f17dfd9e0caedf5e1627c
CapUlDis <capuldis@gmail.com>
2020-09-10 17:21:45 +0300

    Add some com~ponents on Create panel.

--- src/panels/Create.js
+++ src/panels/Create.js
@@ -2,6 +2,13 @@ import React from 'react';
 import Panel from '@vkontakte/vkui/dist/components/Panel/Panel';
 import PanelHeader from '@vkontakte/vkui/dist/components/PanelHeader/PanelHeader';
 import PanelHeaderButton from '@vkontakte/vkui/dist/components/PanelHeaderButton/PanelHeaderButton';
+import FormLayout from '@vkontakte/vkui/dist/components/FormLayout/FormLayout';
+import FormLayoutGroup from '@vkontakte/vkui/dist/components/FormLayoutGroup/FormLayoutGroup';
+import Checkbox from '@vkontakte/vkui/dist/components/Checkbox/Checkbox';
+import Radio from '@vkontakte/vkui/dist/components/Radio/Radio';
+import Input from '@vkontakte/vkui/dist/components/Input/Input';
+import Tabs from '@vkontakte/vkui/dist/components/Tabs/Tabs';
+import TabsItem from '@vkontakte/vkui/dist/components/TabsItem/TabsItem';
 import Icon28Notifications from '@vkontakte/icons/dist/28/notifications';
 
 
@@ -10,7 +17,31 @@ const Create = ({ id }) => (
 	<Panel id={id}>
 		<PanelHeader 
             left={<PanelHeaderButton><Icon28Notifications fill='#4bb34b'/></PanelHeaderButton>}
-            separator={false}>Создать</PanelHeader>
+            separator={false}>Создать
+		</PanelHeader>
+		<FormLayout>
+            <Input
+				type="text"
+				top="Название"
+				name="name"
+				placeholder="Введите название"
+			/>
+			<Input
+				type="date"
+				top="Дата"
+				name="date"
+				placeholder="Выберите дату"
+			/>
+			<FormLayoutGroup top="Как отсчитывать дату?">
+				<Radio name="howCount" value="from">От выбранной даты</Radio>
+				<Radio name="howCount" value="to">До выбранной даты</Radio>
+            </FormLayoutGroup>
+			<Checkbox 
+				top="Дополнительно"
+				name="public"
+			>Сделать счетчик публичным</Checkbox>	
+		</FormLayout>
+
 	</Panel>
 );
 
