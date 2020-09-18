fc6f04ff3b9a723771f5dcc6b61482dca5f375de
CapUlDis <capuldis@gmail.com>
2020-09-18 12:08:05 +0300

    Add Covers.js

---
+++ src/panels/components/img/Covers.js
@@ -0,0 +1,82 @@
+import LoveSmall from './small/love_small.jpg';
+import LoveMedium from './medium/love_medium.jpg';
+import LoveLarge from './large/love_large.jpg';
+import NewYearSmall from './small/newyear_small.jpg';
+import NewYearMedium from './medium/newyear_medium.jpg';
+import NewYearLarge from './large/newyear_large.jpg';
+import MarrySmall from './small/marry_small.jpg';
+import MarryMedium from './medium/marry_medium.jpg';
+import MarryLarge from  './large/marry_large.jpg';
+import PalmSmall from './small/palm_small.jpg';
+import PalmMedium from './medium/palm_medium.jpg';
+import PalmLarge from  './large/palm_large.jpg';
+import FlowersSmall from './small/flowers_small.jpg';
+import FlowersMedium from './medium/flowers_medium.jpg';
+import FlowersLarge from  './large/flowers_large.jpg';
+import StarsSmall from './small/stars_small.jpg';
+import StarsMedium from './medium/stars_medium.jpg';
+import StarsLarge from  './large/stars_large.jpg';
+import RainbowSmall from './small/rainbow_small.jpg';
+import RainbowMedium from './medium/rainbow_medium.jpg';
+import RainbowLarge from  './large/rainbow_large.jpg';
+import SnowflakeSmall from './small/snowflake_small.jpg';
+import SnowflakeMedium from './medium/snowflake_medium.jpg';
+import SnowflakeLarge from  './large/snowflake_large.jpg';
+import AvocadoSmall from './small/avocado_small.jpg';
+import AvocadoMedium from './medium/avocado_medium.jpg';
+import AvocadoLarge from  './large/avocado_large.jpg';
+import CakeSmall from './small/cake_small.jpg';
+import CakeMedium from './medium/cake_medium.jpg';
+import CakeLarge from  './large/cake_large.jpg';
+import BeerSmall from './small/beer_small.jpg';
+import BeerMedium from './medium/beer_medium.jpg';
+import BeerLarge from  './large/beer_large.jpg';
+import BasketballSmall from './small/basketball_small.jpg';
+import BasketballMedium from './medium/basketball_medium.jpg';
+import BasketballLarge from  './large/basketball_large.jpg';
+import MicrophoneSmall from './small/microphone_small.jpg';
+import MicrophoneMedium from './medium/microphone_medium.jpg';
+import MicrophoneLarge from  './large/microphone_large.jpg';
+import CandleSmall from './small/candle_small.jpg';
+import CandleMedium from './medium/candle_medium.jpg';
+import CandleLarge from  './large/candle_large.jpg';
+
+
+const colors = [
+    { id: 1, title: 'blue', style: 'linear-gradient(72.51deg, #5D9DE7 0%, #6FB1FF 100%)' },
+    { id: 2, title: 'magenta', style: 'linear-gradient(72.7deg, #AB67F0 0%, #C18AF6 100%)' },
+    { id: 3, title: 'red', style: 'linear-gradient(72.51deg, #FE3548 0%, #FF727F 100%)' },
+    { id: 4, title: 'cyan', style: 'linear-gradient(72.7deg, #67C2C3 0%, #9AE4E6 100%)' },
+    { id: 5, title: 'orange', style: 'linear-gradient(135deg, #FF9142 0%, #E7750C 100%)' },
+    { id: 6, title: 'green', style: 'linear-gradient(135deg, #50C750 0%, #32B332 100%)' },
+    { id: 7, title: 'gray', style: 'linear-gradient(135deg, #B1B6BD 0%, #99A2AD 100%)' },
+    { id: 8, title: 'yellow', style: 'linear-gradient(135deg, #FFB73D 0%, #FFA000 100%)' },
+    { id: 9, title: 'pink', style: 'linear-gradient(135deg, #FDACCC 0%, #F37AC1 100%)' },
+    { id: 10, title: 'black', style: 'linear-gradient(135deg, #626466 0%, #444647 100%)' }
+];
+
+const images = [
+    { id: 11, title: 'love', small: LoveSmall, medium: LoveMedium, large: LoveLarge },
+    { id: 12, title: 'newyear', small: NewYearSmall, medium: NewYearMedium, large: NewYearLarge },
+    { id: 13, title: 'marry', small: MarrySmall, medium: MarryMedium, large: MarryLarge },
+    { id: 14, title: 'palm', small: PalmSmall, medium: PalmMedium, large: PalmLarge },
+    { id: 15, title: 'flowers', small: FlowersSmall, medium: FlowersMedium, large: FlowersLarge },
+    { id: 16, title: 'stars', small: StarsSmall, medium: StarsMedium, large: StarsLarge },
+    { id: 17, title: 'rainbow', small: RainbowSmall, medium: RainbowMedium, large: RainbowLarge },
+    { id: 18, title: 'snowflake', small: SnowflakeSmall, medium: SnowflakeMedium, large: SnowflakeLarge },
+    { id: 19, title: 'avocado', small: AvocadoSmall, medium: AvocadoMedium, large: AvocadoLarge },
+    { id: 20, title: 'cake', small: CakeSmall, medium: CakeMedium, large: CakeLarge },
+    { id: 21, title: 'beer', small: BeerSmall, medium: BeerMedium, large: BeerLarge },
+    { id: 22, title: 'basketball', small: BasketballSmall, medium: BasketballMedium, large: BasketballLarge },
+    { id: 23, title: 'microphone', small: MicrophoneSmall, medium: MicrophoneMedium, large: MicrophoneLarge },
+    { id: 24, title: 'candle', small: CandleSmall, medium: CandleMedium, large: CandleLarge },
+];
+
+
+
+// function imageLoader() {
+//     return images;
+// }
+
+export { images, colors };
+
